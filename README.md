0d3b178ac5883ae84fb29985c5983785746f7d38
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-02 21:36:56 +1000

    painting worked

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# hirstpainting" 
+100 days of code - Python
+
+Day 18 - Hirst "Dot" Painting
+
+A simple program using the turtle module to create a David Hirst "dot" style painting
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
-import colorgram
+import turtle as turt
+import random
 
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    rgb_colors.append(color.rgb)
+turt.colormode(255)
+timmy = turt.Turtle()
+timmy.speed("fastest")
+timmy.penup()
+timmy.hideturtle()
+color_list = [(202, 164, 109), (238, 240, 245), (150, 75, 49), (223, 201, 135), (52, 93, 124), (172, 154, 40), (140, 30, 19), (133, 163, 185), (198, 91, 71), (46, 122, 86), (72, 43, 35), (145, 178, 148), (13, 99, 71), (233, 175, 164), (161, 142, 158), (105, 74, 77), (55, 46, 50), (183, 205, 171), (36, 60, 74), (18, 86, 90), (81, 148, 129), (148, 17, 20), (14, 70, 64), (30, 68, 100), (107, 127, 153), (174, 94, 97), (176, 192, 209)]
 
-print(rgb_colors)
+timmy.setheading(225)
+timmy.forward(300)
+timmy.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    timmy.dot(20, random.choice(color_list))
+    timmy.forward(50)
+
+    if dot_count % 10 == 0:
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
+screen = turt.Screen()
+screen.exitonclick()
